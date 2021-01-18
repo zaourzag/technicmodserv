b9e805642d402cd9b05f9d56831b1aba089337e1
root <root@llok.mq3vpdadz3duho2rrxyudads3c.syx.internal.cloudapp.net>
2021-01-18 18:02:02 +0000

    import shit v3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/install.sh
-chmod 777 install.sh
+#chmod 777 install.sh
 sh install.sh
 rm install.sh
 wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/init.sh
 wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/start.sh
-chmod 777 init.sh start.sh
+#chmod 777 init.sh start.sh
