479e15552318dbbb41169315a5a45d85e63a790f
root <root@llok.mq3vpdadz3duho2rrxyudads3c.syx.internal.cloudapp.net>
2021-01-18 17:54:48 +0000

    import shit

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/zaourzag/technicmodpack/raw/main/technicserver-all-2.0.2-rc2.jar
-wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/modpack.properties
+wget https://github.com/zaourzag/technicmodserv/raw/main1/technicserver-all-2.0.2-rc2.jar
+wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/modpack.properties
 
 echo "edit modpack.properties with a correct modpack url and then hit start"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/install.sh
+wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/install.sh
 chmod 777 install.sh
 sh install.sh
 rm install.sh
-wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/init.sh
-wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/start.sh
+wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/init.sh
+wget https://raw.githubusercontent.com/zaourzag/technicmodserv/main1/start.sh
 chmod 777 init.sh start.sh
